790e5c9c6f06a8cd121b9290af8d9c2492b4af3c
zkFriendly <zkfriendly@skiff.com>
2024-01-14 19:15:26 +0100

    MockGho token contract

--- src/mocks/MockGho.sol
+++ src/mocks/MockGho.sol
@@ -3,19 +3,17 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MockGho is ERC20, ERC20Burnable, AccessControl {
-    bytes32 public constant FACILITATOR_ROLE = keccak256("MINTER_ROLE");
+contract MockGho is ERC20, ERC20Burnable {
+    bytes32 public constant FACILITATOR_ROLE = keccak256("FACILITATOR_ROLE");
 
-    constructor(address facilitator) ERC20("MOCK GHO", "mGHO") {
-        _grantRole(FACILITATOR_ROLE, facilitator);
+    constructor(address facilitator) ERC20("MOCK GHO", "mGHO") {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
     }
 
-    function mint(
-        address to,
-        uint256 amount
-    ) external onlyRole(FACILITATOR_ROLE) {
-        _mint(to, amount);
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
     }
 }
