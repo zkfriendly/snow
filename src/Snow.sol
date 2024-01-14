5fdf3f9cf657691350f13e5f7ea0415b7dfd60f2
zkFriendly <zkfriendly@skiff.com>
2024-01-14 16:13:40 +0100

    make cross-chain call

--- src/Snow.sol
+++ src/Snow.sol
@@ -2,26 +2,74 @@
 pragma solidity ^0.8.13;
 
 import {IRouterClient} from "@chainlink/contracts-ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/contracts/src/v0.8/ccip/libraries/Client.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip/contracts/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "forge-std/Test.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ILinkFrost} from "./interfaces/ILinkFrost.sol";
+
+import {console2} from "forge-std/Test.sol";
 
 contract Snow {
-    address public immutable GHO; // GHO token address
-    IRouterClient public immutable ROUTER; // chainlink router address
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable GHO; // GHO token address
+    LinkTokenInterface public immutable LINK; // LINK token address
+
+    address public immutable TARGET_FACILITATOR_ADDRESS; // GHO facilitator address on the target chain
     uint64 public immutable TARGET_CHAIN_ID; // target chain id
 
-    constructor(address _GHO, address _ROUTER, uint64 _TARGET_CHAIN_ID) {
-        GHO = _GHO;
-        ROUTER = IRouterClient(_ROUTER);
-        TARGET_CHAIN_ID = _TARGET_CHAIN_ID;
+    IRouterClient public immutable ROUTER; // chainlink router address
+
+    event Frost(address indexed to, uint256 amount, bytes32 forgeId);
+
+    error NotEnoughBalance(uint256 balance, uint256 required);
+
+    constructor(
+        address _gho,
+        address _link,
+        address _targetFacilitatorAddress,
+        address _router,
+        uint64 _targetChainId
+    ) {
+        GHO = IERC20(_gho);
+        LINK = LinkTokenInterface(_link);
+
+        TARGET_FACILITATOR_ADDRESS = _targetFacilitatorAddress;
+        TARGET_CHAIN_ID = _targetChainId;
+
+        ROUTER = IRouterClient(_router);
     }
 
     /// @notice mint GHO on the target chain
     /// @param _to recipient address on the target chain
     /// @param _amount amount of GHO to be minted on the target chain
-    function frost(address _to, uint256 _amount) external {
-        console2.log(msg.sender, address(this), GHO);
+    function frost(address _to, uint256 _amount) external returns (bytes32 frostId) {
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        Client.EVM2AnyMessage memory frostSignal = Client.EVM2AnyMessage({
+            receiver: abi.encode(TARGET_FACILITATOR_ADDRESS), // ABI-encoded receiver address
+            data: abi.encodeWithSelector(ILinkFrost.frost.selector, _to, _amount), // ABI-encoded string
+            tokenAmounts: new Client.EVMTokenAmount[](0), // Empty array indicating no tokens are being sent
+            extraArgs: Client._argsToBytes(
+                // Additional arguments, setting gas limit
+                Client.EVMExtraArgsV1({gasLimit: 200_000})
+                ),
+            // Set the feeToken  address, indicating LINK will be used for fees
+            feeToken: address(LINK)
+        });
 
-        IERC20(GHO).transferFrom(msg.sender, address(this), _amount);
+        uint256 fees = ROUTER.getFee(TARGET_CHAIN_ID, frostSignal);
+
+        if (fees > LINK.balanceOf(address(this))) {
+            revert NotEnoughBalance(LINK.balanceOf(address(this)), fees);
+        }
+
+        LINK.approve(address(ROUTER), fees);
+        frostId = ROUTER.ccipSend(TARGET_CHAIN_ID, frostSignal);
+
+        GHO.safeTransferFrom(msg.sender, address(this), _amount);
+
+        emit Frost(_to, _amount, frostId);
     }
 }
--- test/Snow.t.sol
+++ test/Snow.t.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import {Snow} from "../src/Snow.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 
 contract WormholeTest is Test {
     Snow public snow;
     address public ghoToken = address(1);
-    address public router = address(3);
+    address public linkToken = address(2);
+    address public linkForge = address(3);
+    address public router = address(4);
     uint64 public targetChainId = 2;
 
     function setUp() public {
-        snow = new Snow(ghoToken, router, targetChainId);
+        snow = new Snow(ghoToken, linkToken, linkForge, router, targetChainId);
     }
 
     function test_Setup() public {
@@ -29,6 +32,16 @@ contract WormholeTest is Test {
             abi.encodeWithSelector(IERC20.transferFrom.selector, address(this), address(snow), amount),
             abi.encode(true)
         );
+
+        vm.mockCall(router, abi.encode(IRouterClient.getFee.selector), abi.encode(10)); // set fee to 10
+        vm.mockCall(linkToken, abi.encodeWithSelector(IERC20.balanceOf.selector, address(snow)), abi.encode(100)); // set balance to 100
+        vm.mockCall(
+            linkToken, abi.encodeWithSelector(IERC20.approve.selector, address(router), uint256(10)), abi.encode(true)
+        ); // approve 10
+        vm.mockCall(router, abi.encode(IRouterClient.ccipSend.selector), abi.encode(keccak256("forge"))); // send forge
+
+        vm.expectEmit(address(snow));
+        emit Snow.Frost(address(this), amount, keccak256("forge"));
 
         snow.frost(address(this), amount);
     }
