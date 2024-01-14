bab377579d4903784172b480fca84ea00a6c7633
zkFriendly <zkfriendly@skiff.com>
2024-01-14 22:40:49 +0100

    thaw gho and release collateral on source chain

--- src/Frost.sol
+++ src/Frost.sol
@@ -1,22 +1,76 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {IRouterClient} from "@chainlink/contracts-ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/contracts/src/v0.8/ccip/libraries/Client.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/contracts/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import {IGhoToken} from "./interfaces/IGhoToken.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "forge-std/Test.sol";
 
 contract Frost is CCIPReceiver {
+    using SafeERC20 for IGhoToken;
+
     IGhoToken public immutable GHO; // GHO token address
+    IRouterClient public immutable ROUTER; // chainlink router address
+    uint64 public immutable SOURCE_CHAIN_ID; // source chain id (where collateral is locked)
+    address public immutable SNOW; // snow address on the source chain
+    IERC20 public immutable FEE_TOKEN; // (LINK, WETH) token address
 
-    error duplicateMessage(bytes32 messageId);
+    event Thaw(address indexed to, uint256 amount, bytes32 thawId);
 
-    constructor(address _gho, address _router) CCIPReceiver(_router) {
+    error InvalidSender(bytes32 messageId, address sender, address expectedSender);
+    error NotEnoughBalance(uint256 balance, uint256 required);
+
+    constructor(address _gho, address _router, address _snow, address _feeToken, uint64 _sourceChainId)
+        CCIPReceiver(_router)
+    {
         GHO = IGhoToken(_gho);
+        ROUTER = IRouterClient(_router);
+        SOURCE_CHAIN_ID = _sourceChainId;
+        SNOW = _snow;
+        FEE_TOKEN = IERC20(_feeToken);
     }
 
-    function _ccipReceive(Client.Any2EVMMessage memory frostSignal) internal override {
-        (address to, uint256 amount) = abi.decode(frostSignal.data, (address, uint256));
+    function thaw(address _to, uint256 _amount) external returns (bytes32 thawId) {
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        Client.EVM2AnyMessage memory thawSignal = Client.EVM2AnyMessage({
+            receiver: abi.encode(SNOW), // ABI-encoded receiver address
+            data: abi.encode(_to, _amount), // ABI-encoded string
+            tokenAmounts: new Client.EVMTokenAmount[](0), // Empty array indicating no tokens are being sent
+            extraArgs: Client._argsToBytes(
+                // Additional arguments, setting gas limit
+                Client.EVMExtraArgsV1({gasLimit: 200_000})
+                ),
+            // Set the feeToken  address, indicating LINK will be used for fees
+            feeToken: address(FEE_TOKEN)
+        });
+
+        // calculate and approve cross-chain transaction fee
+        uint256 fee = ROUTER.getFee(SOURCE_CHAIN_ID, thawSignal);
+        if (FEE_TOKEN.balanceOf(address(this)) < fee) revert NotEnoughBalance(FEE_TOKEN.balanceOf(address(this)), fee);
+        IERC20(FEE_TOKEN).approve(address(ROUTER), _amount);
+
+        // send cross-chain message
+        thawId = ROUTER.ccipSend(SOURCE_CHAIN_ID, thawSignal);
+
+        // transfer GHO to this contract and burn it
+        GHO.safeTransferFrom(msg.sender, address(this), _amount);
+        GHO.burn(_amount);
+
+        emit Thaw(_to, _amount, thawId);
+    }
+
+    /// @notice mint GHO upon receiving a cross-chain message
+    /// @param _frostSignal cross-chain message
+    function _ccipReceive(Client.Any2EVMMessage memory _frostSignal) internal override {
+        address sender = abi.decode(_frostSignal.sender, (address));
+
+        if (sender != SNOW) revert InvalidSender(_frostSignal.messageId, sender, SNOW);
+
+        (address to, uint256 amount) = abi.decode(_frostSignal.data, (address, uint256));
         GHO.mint(to, amount);
     }
 }
--- test/Frost.t.sol
+++ test/Frost.t.sol
@@ -5,35 +5,82 @@ import "forge-std/Test.sol";
 import {Frost} from "../src/Frost.sol";
 import {MockGho} from "../src/mocks/MockGho.sol";
 import {Client} from "@chainlink/contracts-ccip/contracts/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IGhoToken} from "../src/interfaces/IGhoToken.sol";
 
 contract FrostTest is Test {
     Frost public frost;
-    MockGho public ghoToken;
+
+    address public ghoToken = address(1);
     address public router = address(2);
-    address snow = address(3);
-    address alice = address(1);
-    address bob = address(4);
+    address public snow = address(3);
+    address public linkToken = address(4);
+
+    uint64 public sourceChainId = 1;
 
     function setUp() public {
-        ghoToken = new MockGho();
-        frost = new Frost(address(ghoToken), router);
+        frost = new Frost(ghoToken, router, snow, linkToken, sourceChainId);
     }
 
     function test_Setup() public {
         assertEq(address(frost.GHO()), address(ghoToken));
+        assertEq(address(frost.ROUTER()), router);
+        assertEq(frost.SOURCE_CHAIN_ID(), sourceChainId);
+        assertEq(address(frost.FEE_TOKEN()), linkToken);
+        assertEq(address(frost.SNOW()), snow);
     }
 
-    function test_MintGho() public {
+    function testFuzz_MintGho(address alice, uint256 amount) public {
+        vm.assume(alice != address(0));
         Client.Any2EVMMessage memory frostSignal = Client.Any2EVMMessage({
             messageId: keccak256("frostSignal"),
-            sourceChainSelector: 0,
+            sourceChainSelector: sourceChainId,
             sender: abi.encode(snow),
-            data: abi.encode(alice, uint256(100)),
+            data: abi.encode(alice, amount),
             destTokenAmounts: new Client.EVMTokenAmount[](0)
         });
 
-        vm.expectCall(address(ghoToken), abi.encodeWithSelector(ghoToken.mint.selector, alice, uint256(100)));
+        vm.mockCall(ghoToken, abi.encodeWithSelector(IGhoToken.mint.selector, alice, amount), abi.encode(true));
+        vm.expectCall(address(ghoToken), abi.encodeWithSelector(IGhoToken.mint.selector, alice, amount));
         vm.prank(router);
         frost.ccipReceive(frostSignal);
+    }
+
+    function testFuzz_BurnGho(address from, address to, uint256 amount) public {
+        vm.assume(from != address(0));
+        Client.EVM2AnyMessage memory thawSignal = Client.EVM2AnyMessage({
+            receiver: abi.encode(snow), // ABI-encoded receiver address
+            data: abi.encode(to, amount), // ABI-encoded string
+            tokenAmounts: new Client.EVMTokenAmount[](0), // Empty array indicating no tokens are being sent
+            extraArgs: Client._argsToBytes(
+                // Additional arguments, setting gas limit
+                Client.EVMExtraArgsV1({gasLimit: 200_000})
+                ),
+            // Set the feeToken  address, indicating LINK will be used for fees
+            feeToken: address(linkToken)
+        });
+        vm.mockCall(router, abi.encodeWithSelector(IRouterClient.getFee.selector), abi.encode(10)); // set fee to 10
+        vm.mockCall(linkToken, abi.encodeWithSelector(IERC20.balanceOf.selector, address(frost)), abi.encode(100)); // set balance to 100
+        vm.mockCall(linkToken, abi.encodeWithSelector(IERC20.approve.selector, router, amount), abi.encode(true));
+        vm.mockCall(router, abi.encodeWithSelector(IRouterClient.ccipSend.selector), abi.encode(keccak256("thaw"))); // send thaw
+        vm.mockCall(
+            ghoToken,
+            abi.encodeWithSelector(IERC20.transferFrom.selector, from, address(frost), amount),
+            abi.encode(true)
+        );
+        vm.mockCall(ghoToken, abi.encodeWithSelector(IGhoToken.burn.selector, amount), abi.encode(true));
+
+        vm.expectCall(linkToken, abi.encodeWithSelector(IERC20.approve.selector, router, amount)); // set balance to 100
+        vm.expectCall(router, abi.encodeWithSelector(IRouterClient.ccipSend.selector, sourceChainId, thawSignal)); // send thaw
+        vm.expectCall(
+            address(ghoToken), abi.encodeWithSelector(IERC20.transferFrom.selector, from, address(frost), amount)
+        );
+        vm.expectCall(address(ghoToken), abi.encodeWithSelector(IGhoToken.burn.selector, amount));
+        vm.expectEmit(address(frost));
+        emit Frost.Thaw(to, amount, keccak256("thaw"));
+
+        vm.prank(from);
+        frost.thaw(to, amount);
     }
 }
